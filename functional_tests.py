5d87f10d850856f8dc560bb22121fc2c0048f83d
Franciszek Jaworski <franciszekjaworski@gmail.com>
2020-01-18 14:40:42 +0100

    Add  first specification of a functional test in comments and it was accustomed to use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Edith found out a new app to manage a todo list
+        # She decided to visit the main page for the app
+        self.browser.get('http://localhost:8000')
+
+        # She noticed that the page title and header contain word 'Lists'
+        self.assertIn('Lists', self.browser.title)
+        self.fail('End of test!')
+
+        # She immediately decides to write some stuff
+
+        # In the text view she wrote 'buy peacock feathers' (Edith's hobby
+        # is making nice baits
+
+        # After hitting enter the page refreshed and is shows
+        # '1: buy peacock feathers' as element of the list
+
+        # There is still a text view to add new items to the list
+        # Edith wrote down 'use peacock feathers to make a bait' (Edith is very meticulous)
+
+        # The page reloaded and now it shows two items on the list
+
+        # Edith was curious if her list will be saved and she noticed
+        # a unique URL generated for her with a description
+
+        # She went for the URL and found her list
+
+        # Content with results she goes to sleep
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
